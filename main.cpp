c92d8aee761acd2bd277504a9867c179c3ade785
Evgenii Borozna <borozna@ya.ru>
2022-05-17 23:21:17 +0300

    1

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+	short int sInt = 32767;
+	int iI = 2147483647;
+	long long iLL = 4000000;
+	char cChr = 'a';
+	bool bFlg = true;
+	float fFlt = 123.456;
+	double dDbl = 9223372036854775.807;
+
+	enum XO { X, O, _ };
+	enum XO Cell[] = { X, O, _ };
+	return 0;
+}
