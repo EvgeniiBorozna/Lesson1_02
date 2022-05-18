516c0d3d74ffbdbd25ef6182033dea87f08bd725
Evgenii Borozna <borozna@ya.ru>
2022-05-18 21:52:01 +0300

    lesson02

--- main.cpp
+++ main.cpp
@@ -10,6 +10,34 @@ int main() {
 	double dDbl = 9223372036854775.807;
 
 	enum XO { X, O, _ };
-	enum XO Cell[] = { X, O, _ };
+	XO Cell[] = { X, O, X };
+
+	struct stMyStruct {
+		int iX;
+		int iY;
+		XO xoValue;
+		char cPlayerName[20];
+	};
+
+	union MyData {
+		int iValue;
+		float fValue;
+		char cValue;
+	};
+	struct MyVariant {
+		MyData MyDataX;
+		int is_int : 1 ;
+		int is_float : 1;
+		int is_char : 1;
+	} stMyVar1 = {12, 1, 0, 0};
+
+	std::cout << stMyVar1.MyDataX.iValue << " " << stMyVar1.is_int << stMyVar1.is_float << stMyVar1.is_char << std::endl;
+	stMyVar1.MyDataX.fValue = 12.2;
+	stMyVar1.is_int = 0;
+	stMyVar1.is_float = 1;
+	std::cout << stMyVar1.MyDataX.fValue << " " << stMyVar1.is_int << stMyVar1.is_float << stMyVar1.is_char << std::endl;
+	stMyVar1 = { 'C', 0, 0, 1 };
+	std::cout << stMyVar1.MyDataX.cValue << " " << stMyVar1.is_int << stMyVar1.is_float << stMyVar1.is_char << std::endl;
+
 	return 0;
 }
